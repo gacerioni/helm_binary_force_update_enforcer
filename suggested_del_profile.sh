65b9a20099452ec5ff60ea94a0c514550f85640e
Gabriel Cerioni <gacerioni@gmail.com>
2022-06-29 15:42:50 -0300

    Update suggested_del_profile.sh

--- suggested_del_profile.sh
+++ suggested_del_profile.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
 
-# Installing a HELM 3 binary
-curl -o /tmp/helm-v3.8.0-linux-amd64.tar.gz https://get.helm.sh/helm-v3.8.0-linux-amd64.tar.gz
-mkdir -p /opt/harness-delegate/custom-client-tools/helm/v3.8.0
-
-tar -zxvf /tmp/helm-v3.8.0-linux-amd64.tar.gz -C /tmp/
-mv /tmp/linux-amd64/helm /opt/harness-delegate/custom-client-tools/helm/v3.8.0/
-chmod 777 /opt/harness-delegate/custom-client-tools/helm/v3.8.0/
-rm -rf /tmp/linux-amd64
-
-
 # Creating the Helper Script
-cat >/opt/harness-delegate/helm_helper.sh <<EOL
+cat >/opt/harness-delegate/kubectl_helper.sh <<EOL
 #!/bin/bash
 
-#REAL_HELM_BINARY_PATH=/usr/local/bin/helm
-REAL_HELM_BINARY_PATH=/opt/harness-delegate/custom-client-tools/helm/v3.8.0/helm
+REAL_KUBECTL_BINARY_PATH=/opt/harness-delegate/client-tools/kubectl/v1.13.2/kubectl
 
-if [[ "\$1" = "repo" && "\$2" = "add" && ! "\$@" =~ "--force-update" ]]; then
-        command="\$REAL_HELM_BINARY_PATH \$@ --force-update"
+if [[ "\$2" = "apply" ]]; then
+        command="\$REAL_KUBECTL_BINARY_PATH \$@ --force"
 else
-        command="\$REAL_HELM_BINARY_PATH \$@"
+        command="\$REAL_KUBECTL_BINARY_PATH \$@"
 fi
 
 \$command
 EOL
 
-chmod 777 /opt/harness-delegate/helm_helper.sh
+chmod 777 /opt/harness-delegate/kubectl_helper.sh
